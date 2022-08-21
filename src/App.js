f8edae85afb2c2c18b13d5077176a8d0f400796d
coachemmy <coachemmyb@gmail.comgit config --global user.email coachemmyb@gmail.comgit config --global user.email coachemmyb@gmail.com>
2022-08-21 23:42:59 +0100

    commit these

--- src/App.js
+++ src/App.js
@@ -1,11 +1,105 @@
-import "./App.scss";
+import "./App.css";
+import React from 'react'
 
-function App() {
+class App extends React.Component {
+  state ={
+    name:'circle'
+  }
+
+  startRotation = () => {
+    this.setState ({
+      name: 'circle start-rotate'
+    });
+
+    setTimeout(() =>{
+      this.setState({
+        name: 'circle start-rotate stop-rotate'
+      });
+    }, Math.floor(Math.random() * 10000) + 1); 
+  }
+  
+
+render() {
   return (
+
+    
     <div>
-      <h1>Hello World!!!</h1>
+    <div className = 'arrow'></div>
+      <ul className = {this.state.name}>
+        <li>
+          <div className = 'text'
+          contentEditable= 'true'
+          spellCheck = 'false'> $0</div>
+        </li>
+
+         <li>
+          <div className = 'text'
+          contentEditable= 'true'
+          spellCheck = 'false'> $10</div>
+        </li>
+
+         <li>
+          <div className = 'text'
+          contentEditable= 'true'
+          spellCheck = 'false'> $0</div>
+        </li>
+
+         <li>
+          <div className = 'text'
+          contentEditable= 'true'
+          spellCheck = 'false'> $1</div>
+        </li>
+
+         <li>
+          <div className = 'text'
+          contentEditable= 'true'
+          spellCheck = 'false'> $0</div>
+        </li>
+
+         <li>
+          <div className = 'text'
+          contentEditable= 'true'
+          spellCheck = 'false'> $10</div>
+        </li>
+
+         <li>
+          <div className = 'text'
+          contentEditable= 'true'
+          spellCheck = 'false'> $0</div>
+        </li>
+        <li>
+          <div className = 'text'
+          contentEditable= 'true'
+          spellCheck = 'false'> $1</div>
+        </li>
+        <li>
+          <div className = 'text'
+          contentEditable= 'true'
+          spellCheck = 'false'> $0</div>
+        </li>
+        <li>
+          <div className = 'text'
+          contentEditable= 'true'
+          spellCheck = 'false'> $100</div>
+        </li>
+
+         <li>
+          <div className = 'text'
+          contentEditable= 'true'
+          spellCheck = 'false'> $0</div>
+        </li>
+
+         <li>
+          <div className = 'text'
+          contentEditable= 'true'
+          spellCheck = 'false'> $1000</div>
+        </li>
+      </ul>
+      <button className = 'spin-buuton' onClick={this.startRotation}>SPIN</button>
     </div>
-  );
+  )
+};
+
 }
 
 export default App;
